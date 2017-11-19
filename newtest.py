dbeb251576ad77debfd80c2dcf56242df5d33d8f
chenhao1-iri <chenhao1-iri@360.cn>
2017-11-19 16:57:51 +0800

    new model

--- newtest.py
+++ newtest.py
@@ -1,6 +1,6 @@
-from src.model.Identify3 import Model
+from src.model.Identify5 import Model
 
 modelPath = "data/model/vgg"
 model = Model(modelPath + "/model", modelPath + "/summary", modelPath + "/input")
-# model.BuildData("image/mark/taptap_small.png", "image/source", modelPath + "/input")
-model.Train(200)
+# model.BuildData("image/mark/taptap_small.png", "image/source/main", modelPath + "/input")
+model.Train(10000000)
